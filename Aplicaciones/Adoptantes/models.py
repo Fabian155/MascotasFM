32c9d8882365d685299c929927602f4bf868d0ba
Israel Trujillo <it231425@gmail.com>
2025-11-02 21:24:52 -0500

    modificacion de los archivos apps models y admin de adoptantes

--- Aplicaciones/Adoptantes/models.py
+++ Aplicaciones/Adoptantes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Adoptante(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.TextField()
+    cedula = models.TextField()
+    direccion = models.TextField()
+    telefono = models.TextField()
+
+    logo = models.FileField(
+        upload_to='cargos',  
+        null=True,            
+        blank=True            
+    )
+
+    pdf = models.FileField(
+        upload_to='pdfs',  
+        blank=True
+    )
+    
+    
+    def __str__(self):
+        return f"{self.nombre} - {self.cedula}"
