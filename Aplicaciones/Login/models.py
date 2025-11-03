3bb63818d2449d4e3097dd613f8844de4dc44c98
Israel Trujillo <it231425@gmail.com>
2025-11-02 21:11:31 -0500

    modificacion del archivo models

--- Aplicaciones/Login/models.py
+++ Aplicaciones/Login/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_ADMIN = 'ADMIN'
+    ROLE_USER = 'USER'
+
+    ROLE_CHOICES = [
+        (ROLE_ADMIN, 'Administrador'),
+        (ROLE_USER, 'Usuario'),
+    ]
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=ROLE_USER
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
