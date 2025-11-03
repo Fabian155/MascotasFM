efe947ea3e2c9be321690e381982231c2a8730b7
Israel Trujillo <it231425@gmail.com>
2025-11-03 12:31:52 -0500

    edicion del archivo views de animalesU

--- Aplicaciones/Usuario/AnimalesU/views.py
+++ Aplicaciones/Usuario/AnimalesU/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from Aplicaciones.Animales.models import AnimalAdoptable  
 
-# Create your views here.
+def lista_animalesU(request):
+    animales = AnimalAdoptable.objects.all()
+    return render(request, 'usuario/inicioU1.html', {'animales': animales})
