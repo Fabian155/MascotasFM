f2ed91ccdc4154af107601be69b51b10bbce7e84
Israel Trujillo <it231425@gmail.com>
2025-11-02 21:23:19 -0500

    modificacion de los archivos apps models y admin de animales

--- Aplicaciones/Animales/models.py
+++ Aplicaciones/Animales/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class AnimalAdoptable(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.TextField()
+    especieid = models.TextField()
+    edad = models.TextField()
+    salud = models.TextField()
+    
+    logo = models.FileField(
+        upload_to='cargos',  
+        null=True,            
+        blank=True            
+    )
+
+    pdf = models.FileField(
+        upload_to='pdfs',  
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.nombre
