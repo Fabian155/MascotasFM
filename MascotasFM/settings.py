a1b4fdd8119e01ff19db6ba93e5a3321357785b7
Israel Trujillo <it231425@gmail.com>
2025-11-02 21:17:24 -0500

    modificacion del archivo settings parte 2

--- MascotasFM/settings.py
+++ MascotasFM/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'Aplicaciones.Login',
 ]
 
+AUTH_USER_MODEL = 'Login.CustomUser'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
